d63709bbaf1c5384f26d900997c76edab6565c93
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-04-10 02:55:33 +0000

    Removing Trunk from Chronolabs Modules

--- trunk/modules/compounds/include/form.objects.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	include_once XOOPS_ROOT_PATH . '/class/xoopsformloader.php';
-	include_once XOOPS_ROOT_PATH . '/modules/compounds/class/formcompound.php';
-	include_once XOOPS_ROOT_PATH . '/modules/compounds/class/formpaypal.php';
-	include_once XOOPS_ROOT_PATH . '/modules/tag/include/formtag.php';
-	
-?>
